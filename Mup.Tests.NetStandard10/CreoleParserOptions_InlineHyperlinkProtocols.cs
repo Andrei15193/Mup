ae446460a8e8e99c47f014dbc092e3f7fef7f4ac
Andrei Fangli <andrei_fangli@outlook.com>
2018-05-06 19:54:19 +0300

    Added readme.md and renamed .NET Core and .NET Standard test projects to contain version information.

---
+++ Mup.Tests.NetStandard10/CreoleParserOptions_InlineHyperlinkProtocols.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+using static Mup.Tests.CreoleParseroptionsTestData;
+
+namespace Mup.Tests.NetStandard10
+{
+    public class CreoleParserOptions_InlineHyperlinkProtocols
+    {
+        private const string _property = (nameof(CreoleParserOptions) + "." + nameof(CreoleParserOptions.InlineHyperlinkProtocols) + ": ");
+
+        [Trait("Class", nameof(CreoleParserOptions))]
+        [Fact(DisplayName = (_property + nameof(ThrowsExceptionWhenSettingsToNull)))]
+        public void ThrowsExceptionWhenSettingsToNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new CreoleParserOptions { InlineHyperlinkProtocols = null });
+        }
+
+        [Trait("Class", nameof(CreoleParserOptions))]
+        [Theory(DisplayName = (_property + nameof(ThrowsExceptionWhenCollectionContainsNullEmptyOrWhiteSpace)))]
+        [MemberData(nameof(InvalidInlineProtocolsTestCases), MemberType = typeof(CreoleParseroptionsTestData))]
+        public void ThrowsExceptionWhenCollectionContainsNullEmptyOrWhiteSpace(string value)
+        {
+            Assert.Throws<ArgumentException>(() => new CreoleParserOptions { InlineHyperlinkProtocols = new[] { value } });
+        }
+
+        [Trait("Class", nameof(CreoleParserOptions))]
+        [Theory(DisplayName = (_property + nameof(SetsProtocols)))]
+        [MemberData(nameof(InlineProtocolsTestCases), MemberType = typeof(CreoleParseroptionsTestData))]
+        public void SetsProtocols(string protocols)
+        {
+            var protocolsList = protocols.Split(',').Select(protocol => protocol.Trim()).ToList();
+
+            var options = new CreoleParserOptions
+            {
+                InlineHyperlinkProtocols = protocolsList
+            };
+
+            Assert.Equal(protocolsList, options.InlineHyperlinkProtocols);
+        }
+    }
+}
---
+++ Mup.Tests.NetStandard10/CreoleParser_ParseAsync_TextReader.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using static Mup.Tests.CreoleToHtmlTestData;
+
+namespace Mup.Tests.NetStandard10
+{
+    public class CreoleParser_ParseAsync_TextReader
+    {
+        private static readonly CreoleParser _parser = new CreoleParser();
+
+        private const string _method = (nameof(CreoleParser) + ".ParseAsync(TextReader): ");
+
+        [Trait("Class", nameof(CreoleParser))]
+        [Fact(DisplayName = (_method + nameof(TryingToParseNullThrowsException)))]
+        public void TryingToParseNullThrowsException()
+        {
+            Assert.Throws<ArgumentNullException>(delegate { _parser.ParseAsync(reader: null); });
+        }
+
+        [Trait("Class", nameof(CreoleParser))]
+        [Theory(DisplayName = (_method + nameof(ParseHeadingsToHtml)))]
+        [MemberData(nameof(HeadingsToHtmlTestCases), MemberType = typeof(CreoleToHtmlTestData))]
+        public async Task ParseHeadingsToHtml(string text, string expectedHtml)
+        {
+            using (var stringReader = new StringReader(text))
+            {
+                var actualHtml = await _parser.ParseAsync(stringReader).With(new HtmlWriterVisitor());
+                Assert.Equal(expectedHtml, actualHtml);
+            }
+        }
+
+        [Trait("Class", nameof(CreoleParser))]
+        [Theory(DisplayName = (_method + nameof(ParseParagraphsToHtml)))]
+        [MemberData(nameof(ParagraphsToHtmlTestCases), MemberType = typeof(CreoleToHtmlTestData))]
+        public async Task ParseParagraphsToHtml(string text, string expectedHtml)
+        {
+            using (var stringReader = new StringReader(text))
+            {
+                var actualHtml = await _parser.ParseAsync(stringReader).With(new HtmlWriterVisitor());
+                Assert.Equal(expectedHtml, actualHtml);
+            }
+        }
+
+        [Trait("Class", nameof(CreoleParser))]
+        [Theory(DisplayName = (_method + nameof(ParsePreforamattedBlocksToHtml)))]
+        [MemberData(nameof(PreformattedBlocksToHtmlTestCases), MemberType = typeof(CreoleToHtmlTestData))]
+        public async Task ParsePreforamattedBlocksToHtml(string text, string expectedHtml)
+        {
+            using (var stringReader = new StringReader(text))
+            {
+                var actualHtml = await _parser.ParseAsync(stringReader).With(new HtmlWriterVisitor());
+                Assert.Equal(expectedHtml, actualHtml);
+            }
+        }
+
+        [Trait("Class", nameof(CreoleParser))]
+        [Theory(DisplayName = (_method + nameof(ParseTablesToHtml)))]
+        [MemberData(nameof(TablesToHtmlTestCases), MemberType = typeof(CreoleToHtmlTestData))]
+        public async Task ParseTablesToHtml(string text, string expectedHtml)
+        {
+            using (var stringReader = new StringReader(text))
+            {
+                var actualHtml = await _parser.ParseAsync(stringReader).With(new HtmlWriterVisitor());
+                Assert.Equal(expectedHtml, actualHtml);
+            }
+        }
+
+        [Trait("Class", nameof(CreoleParser))]
+        [Theory(DisplayName = (_method + nameof(ParseListsToHtml)))]
+        [MemberData(nameof(ListsToHtmlTestsCases), MemberType = typeof(CreoleToHtmlTestData))]
+        public async Task ParseListsToHtml(string text, string expectedHtml)
+        {
+            using (var stringReader = new StringReader(text))
+            {
+                var actualHtml = await _parser.ParseAsync(stringReader).With(new HtmlWriterVisitor());
+                Assert.Equal(expectedHtml, actualHtml);
+            }
+        }
+
+        [Trait("Class", nameof(CreoleParser))]
+        [Theory(DisplayName = (_method + nameof(ParsesEscapeCharactersToHtml)))]
+        [MemberData(nameof(EscapeCharacterToHtmlTestCases), MemberType = typeof(CreoleToHtmlTestData))]
+        public async Task ParsesEscapeCharactersToHtml(string text, string expectedHtml)
+        {
+            using (var stringReader = new StringReader(text))
+            {
+                var actualHtml = await _parser.ParseAsync(stringReader).With(new HtmlWriterVisitor());
+                Assert.Equal(expectedHtml, actualHtml);
+            }
+        }
+
+        [Trait("Class", nameof(CreoleParser))]
+        [Theory(DisplayName = (_method + nameof(ParsesRichTextToHtml)))]
+        [MemberData(nameof(RichTextToHtmlTestCases), MemberType = typeof(CreoleToHtmlTestData))]
+        public async Task ParsesRichTextToHtml(string text, string expectedHtml)
+        {
+            using (var stringReader = new StringReader(text))
+            {
+                var actualHtml = await _parser.ParseAsync(stringReader).With(new HtmlWriterVisitor());
+                Assert.Equal(expectedHtml, actualHtml);
+            }
+        }
+
+        [Trait("Class", nameof(CreoleParser))]
+        [Theory(DisplayName = (_method + nameof(ParsesHorizontalRuleToHtml)))]
+        [MemberData(nameof(HorizontalRuleToHtmlTestsCases), MemberType = typeof(CreoleToHtmlTestData))]
+        public async Task ParsesHorizontalRuleToHtml(string text, string expectedHtml)
+        {
+            using (var stringReader = new StringReader(text))
+            {
+                var actualHtml = await _parser.ParseAsync(stringReader).With(new HtmlWriterVisitor());
+                Assert.Equal(expectedHtml, actualHtml);
+            }
+        }
+
+        [Trait("Class", nameof(CreoleParser))]
+        [Theory(DisplayName = (_method + nameof(ParsesPluginsToHtml)))]
+        [MemberData(nameof(PluginToHtmlTestsCases), MemberType = typeof(CreoleToHtmlTestData))]
+        public async Task ParsesPluginsToHtml(string text, string expectedHtml)
+        {
+            using (var stringReader = new StringReader(text))
+            {
+                var actualHtml = await _parser.ParseAsync(stringReader).With(new HtmlWriterVisitor());
+                Assert.Equal(expectedHtml, actualHtml);
+            }
+        }
+
+        [Trait("Class", nameof(CreoleParser))]
+        [Trait("WebSite", "www.wikicreole.org")]
+        [Theory(DisplayName = (_method + nameof(CreoleSiteTestsCase)))]
+        [MemberData(nameof(CreoleWikiTestsCases), MemberType = typeof(CreoleToHtmlTestData))]
+        public async Task CreoleSiteTestsCase(string text, string expectedHtml)
+        {
+            using (var stringReader = new StringReader(text))
+            {
+                var actualHtml = await _parser.ParseAsync(stringReader).With(new HtmlWriterVisitor());
+                Assert.Equal(expectedHtml, actualHtml);
+            }
+        }
+    }
+}
