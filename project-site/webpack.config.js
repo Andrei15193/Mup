7695f9a50db54c36e35bd3232beef0488b8842b4
Andrei Fangli <andrei_fangli@outlook.com>
2020-05-28 12:51:10 +0300

    #15 Added HTML meta tags
    (closes #15)

--- project-site/webpack.config.js
+++ project-site/webpack.config.js
@@ -58,6 +58,10 @@ module.exports = {
                 <!DOCTYPE html>
                 <html>
                     <head>
+                        <meta charset="utf-8">
+                        <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+                        <meta http-equiv="Pragma" content="no-cache">
+                        <meta http-equiv="Expires" content="0">
                         ${htmlWebpackPlugin.tags.headTags}
                         <title>${htmlWebpackPlugin.options.title}</title>
                         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
