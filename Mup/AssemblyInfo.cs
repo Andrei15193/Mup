31f2caed2b68cf403bd3710228d02986298314d6
Andrei Fangli <andrei_fangli@outlook.com>
2020-06-10 13:44:07 +0300

    #18 Moved package info from nuspec to csproj
    (closes #18)

--- Mup/AssemblyInfo.cs
+++ Mup/AssemblyInfo.cs
@@ -1,17 +1,5 @@
 ﻿using System;
-using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
-
-[assembly: AssemblyTitle("Mup")]
-[assembly: AssemblyCopyright("Andrei Fangli")]
-[assembly: AssemblyDescription("Markup Parser (Mup) is an extensible .NET Standard Library (1.6) containing types for parsing and generating HTML from text using the Creole lightweight markup language.")]
-[assembly: AssemblyProduct("Mup")]
-
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
-
-[assembly: InternalsVisibleTo("Mup.Tests")]
