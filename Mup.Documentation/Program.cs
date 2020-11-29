74d36eca827a71baf63d7b443c76525810fb9b05
Andrei Fangli <andrei_fangli@outlook.com>
2020-11-29 22:33:41 +0200

    Updated to latest CodeMap for generating project site

--- Mup.Documentation/Program.cs
+++ Mup.Documentation/Program.cs
@@ -1,6 +1,9 @@
-﻿using System.IO;
-using System.Text.RegularExpressions;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using CodeMap.DeclarationNodes;
+using CodeMap.Handlebars;
 
 namespace Mup.Documentation
 {
@@ -8,28 +11,26 @@ namespace Mup.Documentation
     {
         internal static void Main()
         {
-            var mupAssembly = typeof(ParseTreeVisitor).Assembly;
-            var mupAssemblyDeclration = DeclarationNode.Create(mupAssembly).Apply(new MupAssemblyDocumentationAddition());
+            var templateWriter = new HandlebarsTemplateWriter(
+                new MemberReferenceResolver(
+                    new Dictionary<Assembly, IMemberReferenceResolver>
+                    {
+                        { typeof(ParseTreeVisitor).Assembly, new CodeMapMemberReferenceResolver() }
+                    },
+                    new MicrosoftDocsMemberReferenceResolver("netstandard-1.0")
+                )
+            );
 
-            var memberReferenceResolver = new CodeMapMemberReferenceResolver();
-            var templateWriter = new CodeMapHandlebarsTemplateWriter(memberReferenceResolver);
-
-            var declarationNodeVisitor = new FileWriterTemplateWriterDeclarationNodeVisitor(memberReferenceResolver, templateWriter);
-
-            foreach (var assetResourceName in typeof(Program).Assembly.GetManifestResourceNames())
-            {
-                var match = Regex.Match(assetResourceName, @"\.Assets\.(?<assetName>\w+\.\w+)$", RegexOptions.IgnoreCase);
-                if (match.Success)
-                    using (var assetFileStream = new FileStream(match.Groups["assetName"].Value, FileMode.Create, FileAccess.Write, FileShare.Read))
-                    using (var assetResourceStream = typeof(Program).Assembly.GetManifestResourceStream(assetResourceName))
-                        assetResourceStream.CopyTo(assetFileStream);
-            }
+            DeclarationNode
+                .Create(typeof(ParseTreeVisitor).Assembly)
+                .Apply(new MupAssemblyDocumentationAddition())
+                .Accept(new HandlebarsWriterDeclarationNodeVisitor(new DirectoryInfo(Environment.CurrentDirectory), templateWriter));
+            File.Move(Path.Combine(Environment.CurrentDirectory, "index.html"), Path.Combine(Environment.CurrentDirectory, "documentation.html"), overwrite: true);
 
             using (var indexPageWriter = new StreamWriter(new FileStream("index.html", FileMode.Create, FileAccess.Write, FileShare.Read)))
                 templateWriter.Write(indexPageWriter, "Index", new { isHomeSelected = true });
             using (var indexPageWriter = new StreamWriter(new FileStream("license.html", FileMode.Create, FileAccess.Write, FileShare.Read)))
                 templateWriter.Write(indexPageWriter, "License", new { isLicenseSelected = true });
-            mupAssemblyDeclration.Accept(declarationNodeVisitor);
         }
     }
 }
